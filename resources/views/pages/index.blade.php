3b0a809252968cf0e857c839940d2f931269b30b
Santiago Dimaren <sdimaren1@gmail.com>
2018-02-01 13:38:19 -0500

    blade templating and compiling assets

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,14 +1,12 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>{{config('app.name', 'LSAPP')}}</title>
-    </head>
-    <body>
-        <h1>Welcome to Laravel</h1>
-        <p>This is the laravel application home page</p>
-    </body>
-</html>
+@section('content')
+  <div class="jumbotron text-center">
+    <h1>{{$title}}</h1>
+    <p>This is the laravel application home page</p>
+    <p>
+      <a class="btn btn-primary btn-lg" href="/login" role="button">Login</a>
+      <a class="btn btn-success btn-lg" href="/register" role="button">Register</a>
+    </p>
+  </div>
+@endsection
